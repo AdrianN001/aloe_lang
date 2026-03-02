4691d82accbb5f7f5b4c124134dfe24fd1e03c15
Adrian <gogosadrian@outlook.com>
2026-03-02 23:10:12 +0100

    filter too

--- src/object/member/array.rs
+++ src/object/member/array.rs
@@ -144,6 +144,11 @@ impl Array {
         }
 
         if let Object::Func(function) = &*args[0].borrow() {
+            if function.parameters.len() > 1 {
+                return Rc::new(RefCell::new(Object::new_error(
+                    "function provided to the map needs more than 1 argument".into(),
+                )))
+            }
             let mut mapped_array_content = Vec::new();
 
             for item in self.items.clone() {
